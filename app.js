e043772cddd6c607d28ea180e914b81bd03feb94
arnausubiros <arnausubiros@gmail.com>
2023-08-03 20:49:46 +0200

    botón loading

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 console.log("funcionando");
 
 const formulario = document.querySelector('#formulario')
+const btnEnviar  = document.querySelector('#btnEnviar')
+const btnLoading = document.querySelector('#btnLoading')
+
+
 
 formulario.addEventListener('submit', (e) => {
-    console.log('me diste click')
-    e.preventDefault()
+  //  console.log('me diste click')
+    e.preventDefault(() => {
+
+    },3000)
 
     const datos = new FormData(formulario)
+
     console.log('campo Email', datos.get('emailCampo'))
     console.log('campo Password', datos.get('passCampo'))
-     console.log('campo Checkbox', datos.get('checkCampo'))
+    console.log('campo Checkbox', datos.get('checkCampo'))
+
+    btnEnviar.classList.add('d-none')
+    btnLoading.classList.remove('d-none')
+
+
+
+    window.setTimeout(() => {   
+        btnEnviar.classList.remove('d-none')
+        btnLoading.classList.add('d-none')
+    },3000)
+
+
+
+
 
     formulario.reset()
     
+
 
 
 
