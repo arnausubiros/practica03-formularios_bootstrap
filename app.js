66ac818acff2688fb308d22a0c2aa7dcbc8e32e9
arnausubiros <arnausubiros@gmail.com>
2023-08-06 21:28:22 +0200

    componente toast

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-console.log("funcionando");
 
-const formulario = document.querySelector('#formulario')
-const btnEnviar  = document.querySelector('#btnEnviar')
-const btnLoading = document.querySelector('#btnLoading')
+console.log('funcionando!')
+
+const formulario = document.querySelector('#formulario');
+const btnEnviar = document.querySelector('#btnEnviar');
+const btnLoading = document.querySelector('#btnLoading');
 
 
-
-formulario.addEventListener('submit', (e) => {
-  //  console.log('me diste click')
-    e.preventDefault(() => {
-
-    },3000)
-
-    const datos = new FormData(formulario)
-
-    console.log('campo Email', datos.get('emailCampo'))
-    console.log('campo Password', datos.get('passCampo'))
-    console.log('campo Checkbox', datos.get('checkCampo'))
-
-    btnEnviar.classList.add('d-none')
-    btnLoading.classList.remove('d-none')
+const toast = document.querySelector('.toast')
 
 
 
-    window.setTimeout(() => {   
+formulario.addEventListener('submit', e => {
+    
+    btnEnviar.classList.add('d-none');
+    btnLoading.classList.remove('d-none');
+    
+    e.preventDefault();
+
+    const datos = new FormData(formulario);
+
+    console.log('email:', datos.get('emailCampo'));
+    console.log('password: ', datos.get('passCampo'));
+    console.log('aceptar: ', datos.get('checkCampo'));
+
+    // Enviar datos según sistema de backend
+    // PHP: https://www.youtube.com/watch?v=nLrL9Ip3tWI
+
+    window.setTimeout(() => {
         btnEnviar.classList.remove('d-none')
         btnLoading.classList.add('d-none')
-    },3000)
 
 
-
-
-
-    formulario.reset()
+         const eventoToast = new bootstrap.Toast(toast)
+         eventoToast.show()
+        
     
 
-
-
+    }, 1000)
 
 })
